bdd7cb4b8f9f442503a3ed6470ab455675ec4843
Sven <sven@139.com>
2019-02-19 10:33:53 +0800

    build(webpack.config.js package.json): add pxtorem plugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const fs = require('fs');
 const path = require('path');
@@ -96,6 +96,11 @@ module.exports = function(webpackEnv) {
                 flexbox: 'no-2009',
               },
               stage: 3,
+            }),
+            require('postcss-pxtorem')({
+              rootValue: 37.5,
+              propWhiteList: ['*'],
+              selectorBlackList: [],
             }),
           ],
           sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
