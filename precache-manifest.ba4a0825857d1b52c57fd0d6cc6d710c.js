5cc80fe32b4e1fe4b0a8157059ed6e738ed7c0d4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-03-30 02:15:59 +0000

    Deploy YutHelloWorld/personal-income-tax-calculator to github.com/YutHelloWorld/personal-income-tax-calculator.git:gh-pages

---
+++ precache-manifest.ba4a0825857d1b52c57fd0d6cc6d710c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d32f547ca79c186c74",
+    "url": "/personal-income-tax-calculator/static/js/runtime~main.f2d32f54.js"
+  },
+  {
+    "revision": "eb2d45814927f7c1a8f1",
+    "url": "/personal-income-tax-calculator/static/js/main.eb2d4581.chunk.js"
+  },
+  {
+    "revision": "ac3f02df79e54d376f79",
+    "url": "/personal-income-tax-calculator/static/js/4.ac3f02df.chunk.js"
+  },
+  {
+    "revision": "f984df3b0ecb2d2fec59",
+    "url": "/personal-income-tax-calculator/static/js/3.f984df3b.chunk.js"
+  },
+  {
+    "revision": "d35f94551c59863c0dad",
+    "url": "/personal-income-tax-calculator/static/js/2.d35f9455.chunk.js"
+  },
+  {
+    "revision": "eb2d45814927f7c1a8f1",
+    "url": "/personal-income-tax-calculator/static/css/main.b8da5d2f.chunk.css"
+  },
+  {
+    "revision": "f4e92e614bf366092bfc0f74e5181875",
+    "url": "/personal-income-tax-calculator/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/personal-income-tax-calculator/precache-manifest.ba4a0825857d1b52c57fd0d6cc6d710c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/personal-income-tax-calculator/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
